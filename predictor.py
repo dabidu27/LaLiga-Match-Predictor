387c049fb07367a7528e7a3432508aeb9d8530bd
david <adamita.david@gmail.com>
2025-10-09 14:27:19 +0300

    Added more predictors to improve model accuracy

--- predictor.py
+++ predictor.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV, KFold
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
@@ -70,6 +70,7 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
+"""
 #Logistic Regression model
 LR = LogisticRegression(max_iter=1000)
 LR.fit(X_train_scaled, y_train)
@@ -128,3 +129,97 @@ print('XGBoost Evaluation: ')
 print('Accuracy: ', accuracy_score(y_test, y_pred))
 print('Confusion matrix: ', confusion_matrix(y_test, y_pred))
 print('Classification report: ', classification_report(y_test, y_pred))
+"""
+
+#MORE FEATURE ENGINEERING
+#points per game
+
+points = {'W': 3, 'D': 1, 'L': 0}
+matches_df['Points'] = matches_df['Result'].map(points)
+print(matches_df.head())
+
+#rolling averages
+
+grouped_matches = matches_df.groupby('Name')
+#group = grouped_matches.get_group('Real Madrid')
+#print(group)
+
+def rolling_averages(group, cols, new_col):
+
+    group = group.sort_values('Date')
+    rolling_stats = group[cols].rolling(5, closed = 'left').mean()
+    group[new_col] = rolling_stats
+    group = group.dropna(subset = new_col)
+    return group
+
+
+
+cols = ['GF', 'GA', 'Sh', 'SoT', 'Dist', 'FK', 'PK', 'PKatt', 'Points']
+new_cols = [f"{c}_roll" for c in cols]
+
+matches_df_rolling = matches_df.groupby('Name').apply(lambda x: rolling_averages(x, cols, new_cols))
+matches_df_rolling = matches_df_rolling.droplevel('Name')
+matches_df_rolling.index = range(matches_df_rolling.shape[0])
+print(matches_df_rolling)
+
+#test models
+features = ['Venue_code', 'Opponent_code', 'Hour', 'Day_code', 'GF_roll', 'GA_roll', 'Sh_roll', 'SoT_roll', 'Dist_roll', 'FK_roll', 'PK_roll', 'PKatt_roll', 'Points_roll']
+
+X = matches_df_rolling[features]
+X.columns = X.columns.astype(str)
+
+y = matches_df_rolling['target']
+
+#Logistic Regression
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+lr = LogisticRegression(max_iter=1000)
+lr.fit(X_train_scaled, y_train)
+y_pred = lr.predict(X_test_scaled)
+
+print('Logistic Regression Evaluation: ')
+print('Accuracy: ', accuracy_score(y_test, y_pred))
+print('Confussion matrix: ', confusion_matrix(y_test, y_pred))
+print('Classification report: ', classification_report(y_test, y_pred))
+
+coef_importance = pd.DataFrame({'features': features, 'coefficient': lr.coef_[0]}).sort_values('coefficient', ascending=False)
+print(coef_importance)
+
+#Random Forest
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+rf = RandomForestClassifier(random_state=42)
+
+param_grid = {'n_estimators': [50, 100, 200], 'min_samples_split': [2, 5, 10]}
+rf_cv = GridSearchCV(rf, param_grid=param_grid, cv = kf)
+
+rf_cv.fit(X_train, y_train)
+best_rf = rf_cv.best_estimator_
+y_pred = best_rf.predict(X_test)
+
+print('Random Forest Evaluation: ')
+print('Accuracy: ', accuracy_score(y_test, y_pred))
+
+#XGBoost
+
+xgb = XGBClassifier(random_state = 42, use_label_encoder = False, eval_metric = 'logloss')
+
+params_grid = {'n_estimators': [100, 200, 300], 'max_depth': [3,5,7], 'learning_rate': [0.01, 0.1, 0.2], 'subsample': [0.8, 1.0]}
+xgb_cv = GridSearchCV(xgb, params_grid, cv = kf)
+xgb_cv.fit(X_train, y_train)
+
+best_xgb = xgb_cv.best_estimator_
+y_pred = best_xgb.predict(X_test)
+
+print('XGBoost Evaluation: ')
+print('Accuracy: ', accuracy_score(y_test, y_pred))
+
+
+#After evaluation, Logistic Regression proved to be the best model
+
+#elo rating
